e2d07f3091865917290e75a030c12c9df069de85
Maxence Laurent <maxence.laurent@gmail.com>
2021-03-05 09:02:57 +0100

    REmove unused import Signed-off-by:Maxence Laurent <maxence.laurent@gmail.com>

--- client-generator-plugin/src/main/java/ch/colabproject/colab/generator/TypeScriptHelper.java
+++ client-generator-plugin/src/main/java/ch/colabproject/colab/generator/TypeScriptHelper.java
@@ -18,8 +18,6 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.Map;
 import java.util.Set;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import java.util.stream.Collectors;
 import javax.json.Json;
 import javax.json.JsonObject;
