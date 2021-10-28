48fa40f4a060effd33b7e84e4ef70abf27ea53f3
Maxence Laurent <maxence.laurent@gmail.com>
2021-10-28 15:14:49 +0200

    No longer complains about unused variables and parameters during development

--- colab-webapp/src/main/node/app/webpack.config.js
+++ colab-webapp/src/main/node/app/webpack.config.js
@@ -1,59 +1,72 @@
 const path = require('path');
-
-module.exports = {
-    entry: './src/components/App.tsx',
-    devtool: 'inline-source-map',
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/
-            }, {
-                test: /\.svg$/,
-                use: ['@svgr/webpack']
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    'style-loader','css-loader'
-                ]
-            }
+const config = {
+  entry: './src/components/App.tsx',
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: {
+          loader: 'ts-loader'
+        },
+        exclude: /node_modules/
+      }, {
+        test: /\.svg$/,
+        use: ['@svgr/webpack']
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader', 'css-loader'
         ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, '../../../../target/coLAB/dist'),
+    publicPath: './dist'
+  },
+  devServer: {
+    host: '0.0.0.0',
+    port: 3004,
+    proxy: {
+      '/favicon_128.png': 'http://localhost:8080',
+      '/api': 'http://localhost:8080',
+      '/ws': {
+        target: 'ws://localhost:8080',
+        ws: true
+      }
     },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js']
+    client: {
+      overlay: true,
+      webSocketURL: {
+        hostname: "0.0.0.0",
+        pathname: "/webpackws",
+        port: 0,
+      },
     },
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, '../../../../target/coLAB/dist'),
-        publicPath: './dist'
+    devMiddleware: {
+      stats: 'errors-warnings',
+      publicPath: '/dist',
     },
-    devServer: {
-        host: '0.0.0.0',
-        port: 3004,
-        proxy: {
-            '/favicon_128.png': 'http://localhost:8080',
-            '/api': 'http://localhost:8080',
-            '/ws': {
-                target: 'ws://localhost:8080',
-                ws: true
-            }
-        },
-        client:{
-            overlay: true,
-            webSocketURL: {
-                hostname: "0.0.0.0",
-                pathname: "/webpackws",
-                port: 0,
-            },
-        },
-        devMiddleware: {
-            stats: 'errors-warnings',
-            publicPath: '/dist',
-        },
-        webSocketServer: {
-            options: {path: '/webpackws'}
-        }
+    webSocketServer: {
+      options: {path: '/webpackws'}
     }
+  }
+};
+module.exports = (env, argv) => {
+  if (argv.mode === 'development') {
+    config.module.rules[0].use.options = {
+      compilerOptions: {
+        "noUnusedLocals": false,
+        "noUnusedParameters": false
+      }
+    };
+  }
+
+  return config;
 };
