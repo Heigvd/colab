4fb6f0bc3372aaa2cce346fd67b79872ef1489c0
Maxence Laurent <maxence.laurent@gmail.com>
2022-02-08 08:57:49 +0100

    [Markdown] todo list style Signed-off-by:Maxence Laurent <maxence.laurent@gmail.com>

--- colab-webapp/src/main/node/app/src/components/blocks/markdown/MarkdownViewer.tsx
+++ colab-webapp/src/main/node/app/src/components/blocks/markdown/MarkdownViewer.tsx
@@ -4,18 +4,36 @@
  *
  * Licensed under the MIT License
  */
-import { css, cx } from '@emotion/css';
+import {css, cx} from '@emotion/css';
 import * as React from 'react';
 import logger from '../../../logger';
-import { space_S } from '../../styling/style';
+import {space_S} from '../../styling/style';
 import markdownToDom from './parser/markdownToDom';
+
+
+export const colabFlavouredMarkdown = css({
+  "li[data-checked='TODO']": {
+    listStyleType: 'none',
+    "::before": {
+      content: '"\\2610  "', // \f0c8
+      // fontFamily: 'FontAwesome', \\ f0c8
+    }
+  },
+  " li[data-checked='DONE']": {
+    listStyleType: 'none',
+    "::before": {
+      content: '"\\1F5F9  "', // \f14a
+      //fontFamily: 'FontAwesome',  \f14a
+    }
+  },
+});
 
 export interface MarkdownViewerProps {
   md: string;
   className?: string;
 }
 
-export default function MarkdownViewer({ md, className }: MarkdownViewerProps): JSX.Element {
+export default function MarkdownViewer({md, className}: MarkdownViewerProps): JSX.Element {
   const divRef = React.useRef<HTMLDivElement>(null);
 
   React.useEffect(() => {
@@ -34,9 +52,9 @@ export default function MarkdownViewer({ md, className }: MarkdownViewerProps): 
   }, [md]);
 
   return (
-    <div className={cx(css({ p: { margin: space_S + ' 0' } }), className)}>
+    <div className={cx(css({p: {margin: space_S + ' 0'}}), colabFlavouredMarkdown, className)}>
       {md === '' ? (
-        <p className={css({ color: 'var(--lightGray)' })}>
+        <p className={css({color: 'var(--lightGray)'})}>
           <i>empty</i>
         </p>
       ) : null}
