65e57a964a58a3bc75d69a208c89f6235d665334
Audrey Huguenin <audreyhugueninbenjamin@gmail.com>
2022-03-29 14:57:15 +0200

    improved resource display

--- colab-webapp/src/main/node/app/src/components/resources/ResourceDisplay.tsx
+++ colab-webapp/src/main/node/app/src/components/resources/ResourceDisplay.tsx
@@ -5,14 +5,22 @@
  * Licensed under the MIT License
  */
 import { css, cx } from '@emotion/css';
-import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+import { faArrowLeft, faCog, faTrash, faTrashAlt } from '@fortawesome/free-solid-svg-icons';
 import * as React from 'react';
+import * as API from '../../API/api';
+import { updateDocumentText } from '../../API/api';
 import useTranslations from '../../i18n/I18nContext';
 import { useAndLoadTextOfDocument } from '../../selectors/documentSelector';
+import { useAppDispatch } from '../../store/hooks';
+import { Destroyer } from '../common/Destroyer';
 import Flex from '../common/Flex';
 import IconButton from '../common/IconButton';
+import InlineInput from '../common/InlineInput';
+import OpenCloseModal from '../common/OpenCloseModal';
+import DocTextWrapper from '../documents/DocTextWrapper';
 import DocumentList from '../documents/DocumentList';
 import {
+  cardTitle,
   lightIconButtonStyle,
   paddingAroundStyle,
   space_M,
@@ -20,6 +28,7 @@ import {
   textSmall,
 } from '../styling/style';
 import { ResourceAndRef } from './ResourceCommonType';
+import { ResourceSettings } from './ResourceMiniDisplay';
 
 export interface ResourceDisplayProps {
   resourceAndRef: ResourceAndRef;
@@ -33,7 +42,7 @@ export function ResourceDisplay({ resourceAndRef, onClose }: ResourceDisplayProp
 
   const allowEdition = resourceAndRef.isDirectResource;
   const { text: teaser } = useAndLoadTextOfDocument(resourceAndRef.targetResource.teaserId);
-
+  const dispatch = useAppDispatch();
   return (
     <Flex
       align="stretch"
@@ -41,21 +50,100 @@ export function ResourceDisplay({ resourceAndRef, onClose }: ResourceDisplayProp
       grow={1}
       className={paddingAroundStyle([2, 3, 4], space_M)}
     >
-      <Flex direction="column">
-        <IconButton
-          icon={faArrowLeft}
-          title="Back"
-          onClick={onClose}
-          className={cx(lightIconButtonStyle, css({ paddingBottom: space_S }))}
-        />
+      <Flex direction="column" align="normal">
+        <Flex
+          justify="space-between"
+          align="center"
+          grow={1}
+          className={css({ marginBottom: space_S })}
+        >
+          <IconButton
+            icon={faArrowLeft}
+            title="Back"
+            onClick={onClose}
+            className={lightIconButtonStyle}
+          />
+          <OpenCloseModal
+            title="Resource Settings"
+            showCloseButton={true}
+            collapsedChildren={
+              <IconButton icon={faCog} className={lightIconButtonStyle} title="Resource settings" />
+            }
+          >
+            {() => (
+              <>
+                <ResourceSettings {...resourceAndRef} />
+                {resourceAndRef.isDirectResource && (
+                  <Destroyer
+                    title="Delete this resource"
+                    icon={faTrashAlt}
+                    onDelete={() => {
+                      dispatch(API.deleteResource(resourceAndRef.targetResource));
+                    }}
+                  />
+                )}
+                {!resourceAndRef.isDirectResource && resourceAndRef.cardTypeResourceRef && (
+                  <Destroyer
+                    title="Refuse at card type level"
+                    icon={faTrash}
+                    onDelete={() => {
+                      dispatch(API.removeAccessToResource(resourceAndRef.cardTypeResourceRef!));
+                    }}
+                  />
+                )}
+                {!resourceAndRef.isDirectResource && resourceAndRef.cardResourceRef && (
+                  <Destroyer
+                    title="Refuse at card level"
+                    icon={faTrash}
+                    onDelete={() => {
+                      dispatch(API.removeAccessToResource(resourceAndRef.cardResourceRef!));
+                    }}
+                  />
+                )}
+                {!resourceAndRef.isDirectResource && resourceAndRef.cardContentResourceRef && (
+                  <Destroyer
+                    title="Refuse at variant level"
+                    icon={faTrash}
+                    onDelete={() => {
+                      dispatch(API.removeAccessToResource(resourceAndRef.cardContentResourceRef!));
+                    }}
+                  />
+                )}
+              </>
+            )}
+          </OpenCloseModal>
+        </Flex>
         <div>
-          <h2>{resourceAndRef.targetResource.title || i18n.resource.untitled}</h2>
+          <InlineInput
+            onChange={newValue =>
+              dispatch(API.updateResource({ ...resourceAndRef.targetResource, title: newValue }))
+            }
+            value={resourceAndRef.targetResource.title || i18n.resource.untitled}
+            placeholder={i18n.resource.untitled}
+            autosave={false}
+            className={cardTitle}
+          />
           {teaser && (
-            <div className={textSmall}>
-              <p>
-                <b>Teaser:</b> {teaser}
-              </p>
-            </div>
+          <DocTextWrapper id={resourceAndRef.targetResource.teaserId}>
+            {text => (
+              <InlineInput
+                placeholder={'Add a teaser'}
+                value={text || 'Add a teaser'}
+                onChange={(newValue: string) => {
+                  if (resourceAndRef.targetResource.teaserId) {
+                    dispatch(
+                      updateDocumentText({
+                        id: resourceAndRef.targetResource.teaserId,
+                        textData: newValue,
+                      }),
+                    );
+                  }
+                }}
+                autosave={false}
+                className={cx(textSmall, css({marginTop: space_S}))}
+              />
+            )}
+          </DocTextWrapper>
           )}
         </div>
       </Flex>
