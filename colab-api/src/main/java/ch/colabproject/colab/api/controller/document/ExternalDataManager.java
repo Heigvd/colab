3a7734e2fe79136909ef676f2429e3919780cecc
Maxence Laurent <maxence.laurent@gmail.com>
2022-10-10 19:51:55 +0200

    Bypass OpenGraph resolution for internal links Signed-off-by:Maxence Laurent <maxence.laurent@gmail.com>

--- colab-api/src/main/java/ch/colabproject/colab/api/controller/document/ExternalDataManager.java
+++ colab-api/src/main/java/ch/colabproject/colab/api/controller/document/ExternalDataManager.java
@@ -6,10 +6,12 @@
  */
 package ch.colabproject.colab.api.controller.document;
 
+import ch.colabproject.colab.api.controller.RequestManager;
 import ch.colabproject.colab.api.rest.document.bean.UrlMetadata;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.net.URI;
+import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
 import java.time.OffsetDateTime;
 import java.util.HashMap;
@@ -19,6 +21,7 @@ import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.text.StringEscapeUtils;
 import org.apache.hc.client5.http.classic.methods.HttpGet;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
 import org.apache.hc.core5.http.Header;
@@ -51,11 +54,17 @@ public class ExternalDataManager {
     /** Open graph title */
     private static final String OG_URL = "og:url";
 
+    /** Open graph image */
+    private static final String OG_IMAGE = "og:image";
+
     /**
      * cache metadata to avoid spamming external services.
      */
     @Inject
     private Cache<String, UrlMetadata> metadataCache;
+
+    @Inject
+    private RequestManager requestManager;
 
     /**
      * Read response entity as stream
@@ -145,71 +154,84 @@ public class ExternalDataManager {
      * @return url metadata
      */
     public UrlMetadata refreshAndGetUrlMetadata(String url) {
+
         UrlMetadata urlMetadata = new UrlMetadata();
         urlMetadata.setBroken(true);
         HashMap<String, String> metadata = new HashMap<>();
         urlMetadata.setMetadata(metadata);
 
-        logger.trace("Raw URL {}", url);
-        try ( var client = HttpClients.createDefault()) {
-            String sanitizedUrl = sanitizeUrl(url, null);
+        String decoded = URLDecoder.decode(url, StandardCharsets.UTF_8);
 
-            URIBuilder uriBuilder = new URIBuilder(sanitizedUrl, StandardCharsets.UTF_8);
+        // hack: intercept loobpack link
+        String baseUrl = requestManager.getBaseUrl();
+        if (decoded.startsWith(baseUrl)) {
+            logger.trace("Loopback url intercepted");
+            urlMetadata.setBroken(false);
+            metadata.put(OG_IMAGE, baseUrl + "/favicon_128.png");
+            metadata.put(OG_URL, decoded);
+        } else {
 
-            URI uri = uriBuilder.normalizeSyntax().build();
-            metadata.put(OG_URL, url);
+            logger.trace("Raw URL {}", url);
+            try ( var client = HttpClients.createDefault()) {
+                String sanitizedUrl = sanitizeUrl(url, null);
 
-            String[] segs = uri.getPath().split("/");
-            if (segs != null && segs.length > 0) {
-                // default og:name to last path segment
-                String filename = segs[segs.length - 1];
-                metadata.put(OG_TITLE, filename);
-            } else {
-                // otherwise, default to hostname
-                metadata.put(OG_TITLE, uri.getHost());
-            }
+                URIBuilder uriBuilder = new URIBuilder(sanitizedUrl, StandardCharsets.UTF_8);
 
-            var get = new HttpGet(uri);
-            try ( var response = client.execute(get)) {
+                URI uri = uriBuilder.normalizeSyntax().build();
+                metadata.put(OG_URL, url);
 
-                HttpEntity entity = response.getEntity();
-                int statusCode = response.getCode();
-
-                if (statusCode < 400) {
-                    // success
-                    urlMetadata.setBroken(false);
-
-                    Header firstHeader = response.getFirstHeader("content-type");
-                    String contentType = firstHeader.getValue();
-                    int separator = contentType.indexOf(';');
-
-                    if (separator > 0) {
-                        contentType = contentType.substring(0, separator);
-                    }
-
-                    if (contentType != null) {
-                        urlMetadata.setContentType(contentType);
-                        if (contentType.equals("text/html")) {
-                            // try to fetch metadata in head meta tags
-                            String html = getEntityAsString(entity);
-                            Document htmlDocument = Jsoup.parse(html, url);
-                            Elements metas = htmlDocument.head().select("meta");
-                            metas.forEach(meta -> {
-                                String prop = meta.attr("property");
-                                String name = meta.attr("name");
-                                if (prop != null && prop.indexOf(':') >= 0
-                                    || name != null && name.indexOf(':') >= 0) {
-                                    metadata.put(prop, meta.attr("content"));
-                                }
-                            });
-                        }
-                    }
+                String[] segs = uri.getPath().split("/");
+                if (segs != null && segs.length > 0) {
+                    // default og:name to last path segment
+                    String filename = segs[segs.length - 1];
+                    metadata.put(OG_TITLE, filename);
+                } else {
+                    // otherwise, default to hostname
+                    metadata.put(OG_TITLE, uri.getHost());
                 }
 
+                var get = new HttpGet(uri);
+                try ( var response = client.execute(get)) {
+
+                    HttpEntity entity = response.getEntity();
+                    int statusCode = response.getCode();
+
+                    if (statusCode < 400) {
+                        // success
+                        urlMetadata.setBroken(false);
+
+                        Header firstHeader = response.getFirstHeader("content-type");
+                        String contentType = firstHeader.getValue();
+                        int separator = contentType.indexOf(';');
+
+                        if (separator > 0) {
+                            contentType = contentType.substring(0, separator);
+                        }
+
+                        if (contentType != null) {
+                            urlMetadata.setContentType(contentType);
+                            if (contentType.equals("text/html")) {
+                                // try to fetch metadata in head meta tags
+                                String html = getEntityAsString(entity);
+                                Document htmlDocument = Jsoup.parse(html, url);
+                                Elements metas = htmlDocument.head().select("meta");
+                                metas.forEach(meta -> {
+                                    String prop = meta.attr("property");
+                                    String name = meta.attr("name");
+                                    if (prop != null && prop.indexOf(':') >= 0
+                                        || name != null && name.indexOf(':') >= 0) {
+                                        metadata.put(prop, meta.attr("content"));
+                                    }
+                                });
+                            }
+                        }
+                    }
+
+                }
+            } catch (Exception e) {
+                logger.debug("Major Failure", e);
+                urlMetadata.setBroken(true);
             }
-        } catch (Exception e) {
-            logger.debug("Major Failure", e);
-            urlMetadata.setBroken(true);
         }
         urlMetadata.setDate(OffsetDateTime.now());
         // cache metadata
@@ -225,7 +247,7 @@ public class ExternalDataManager {
         while (iterator.hasNext()) {
             Cache.Entry<String, UrlMetadata> entry = iterator.next();
             UrlMetadata data = entry.getValue();
-            if (isOutdated(data)){
+            if (isOutdated(data)) {
                 iterator.remove();
             }
         }
