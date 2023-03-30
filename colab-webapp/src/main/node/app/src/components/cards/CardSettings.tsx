8e86845cedc0bb125bd0eb0cd8b14b52151e5e0b
Sandra Monnier <sandra.monnier@heig-vd.ch>
2023-03-30 14:48:27 +0200

    renaming

--- colab-webapp/src/main/node/app/src/components/cards/CardSettings.tsx
+++ colab-webapp/src/main/node/app/src/components/cards/CardSettings.tsx
@@ -19,7 +19,7 @@ import Checkbox from '../common/element/Checkbox';
 import Tips, { WIPContainer } from '../common/element/Tips';
 import Flex from '../common/layout/Flex';
 import Icon from '../common/layout/Icon';
-import ContentStatusSelector from './ContentStatusSelector';
+import ContentStatusSelector from './CardContentStatusSelector';
 
 const marginDownSmall = css({
   marginBottom: 0,
