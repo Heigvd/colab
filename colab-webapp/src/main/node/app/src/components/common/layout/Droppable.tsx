34fb61629c236a4c38ca745406b01b1bcadfb462
Mikkel Vestergaard <56039404+TehAwol@users.noreply.github.com>
2023-05-17 12:13:22 +0200

    feat(droppable): add highlights when hovered

--- colab-webapp/src/main/node/app/src/components/common/layout/Droppable.tsx
+++ colab-webapp/src/main/node/app/src/components/common/layout/Droppable.tsx
@@ -4,12 +4,30 @@
  *
  * Licensed under the MIT License
  */
-import { useDroppable } from '@dnd-kit/core';
+import { useDndMonitor, useDroppable } from '@dnd-kit/core';
 import { css } from '@emotion/css';
 import * as React from 'react';
+import logger from '../../../logger';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export default function Droppable(props: any) {
+  const [isHovered, setIsHovered] = React.useState<boolean>(false);
+
+  useDndMonitor({
+    onDragOver(event) {
+      const { active, over } = event;
+      logger.info(props);
+      if (over && over.id == props.id && active.id && active.id != props.data.cardId) {
+        setIsHovered(true);
+      } else {
+        setIsHovered(false);
+      }
+    },
+    onDragEnd() {
+      setIsHovered(false);
+    },
+  });
+
   const Element = props.element || 'div';
   const { setNodeRef } = useDroppable({
     id: props.id,
@@ -17,7 +35,7 @@ export default function Droppable(props: any) {
   });
 
   return (
-    <Element ref={setNodeRef} className={props.isDragging && css({ border: '4px solid orange' })}>
+    <Element ref={setNodeRef} className={isHovered && css({ border: '1px solid orange' })}>
       {props.children}
     </Element>
   );
