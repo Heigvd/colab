789581cc7dda076e09ae24186685be402744884d
Mikkel Vestergaard <56039404+TehAwol@users.noreply.github.com>
2023-07-11 16:26:32 +0200

    Fix Editor ListView import

--- colab-webapp/src/main/node/app/src/components/projects/edition/Editor.tsx
+++ colab-webapp/src/main/node/app/src/components/projects/edition/Editor.tsx
@@ -27,7 +27,7 @@ import TeamTabs from '../../team/TeamTabs';
 import ActivityFlowChart from '../activityFlow/ActivityFlowChart';
 import DocumentationTab from '../DocumentationTab';
 import Hierarchy from '../hierarchy/Hierarchy';
-import ListViewRoot from '../ListView/ListViewRoot';
+import ListViewRoot from '../listView/ListViewRoot';
 import { ProjectNav } from '../ProjectNav';
 import { ProjectSettingsTabs } from '../settings/ProjectSettingsTabs';
 import ProjectSidePanelWrapper from '../SidePanelWrapper';
