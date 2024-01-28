7151efbe8f0eed1e95217c11cacfd65ba5ea7f8f
Mikkel Vestergaard <56039404+TehAwol@users.noreply.github.com>
2024-01-28 15:52:43 +0100

    Frontend corrections

--- colab-webapp/src/main/node/app/src/components/admin/CronJobMonitor.tsx
+++ colab-webapp/src/main/node/app/src/components/admin/CronJobMonitor.tsx
@@ -5,88 +5,103 @@
  * Licensed under the MIT License
  */
 
-import * as React from "react";
-import {space_2xl, text_xs} from "../../styling/style";
-import {css} from "@emotion/css";
-import {useAppDispatch, useAppSelector} from "../../store/hooks";
+import * as React from 'react';
+import { space_2xl, text_xs } from '../../styling/style';
+import { css } from '@emotion/css';
+import { useAppDispatch, useAppSelector } from '../../store/hooks';
 import * as API from '../../API/api';
-import logger from "../../logger";
-import IconButton from "../common/element/IconButton";
+import logger from '../../logger';
+import IconButton from '../common/element/IconButton';
+import { CronJobLog } from 'colab-rest-client/dist/ColabClient';
+import AvailabilityStatusIndicator from '../common/element/AvailabilityStatusIndicator';
 
 const tableStyle = css({
-    textAlign: 'left',
-    borderCollapse: 'collapse',
-    'td, th': {
-        padding: '10px',
-        overflow: 'hidden',
-        textOverflow: 'ellipsis',
-        whiteSpace: 'nowrap',
-        fontSize: text_xs,
-    },
-})
+  textAlign: 'left',
+  borderCollapse: 'collapse',
+  'td, th': {
+    padding: '10px',
+    overflow: 'hidden',
+    textOverflow: 'ellipsis',
+    whiteSpace: 'nowrap',
+    fontSize: text_xs,
+  },
+});
 
 const headerStyle = css({
-    fontWeight: 'bold',
-    height: space_2xl,
+  fontWeight: 'bold',
+  height: space_2xl,
 });
 
 export default function CronJobMonitor(): React.ReactElement {
-    const dispatch = useAppDispatch();
+  const dispatch = useAppDispatch();
 
-    const cronJobLogs = useAppSelector(state => state.admin.cronJobLogs);
+  const data = useAppSelector(state => state.admin.cronJobLogs);
 
-    React.useEffect(() => {
-        logger.info(cronJobLogs);
-        if (cronJobLogs === 'NOT_INITIALIZED') {
-            dispatch(API.getCronJobLogs());
-        }
-    }, [cronJobLogs, dispatch])
-
-    const sync = React.useCallback(() => {
-        dispatch(API.getCronJobLogs());
-    }, [dispatch])
-
-    if (cronJobLogs === 'LOADING' || cronJobLogs === 'NOT_INITIALIZED') {
-        return (
-            <div>Loading</div>
-        )
+  React.useEffect(() => {
+    logger.info(data);
+    if (data === 'NOT_INITIALIZED') {
+      dispatch(API.getCronJobLogs());
     }
+  }, [data, dispatch]);
 
-    return (
-        <div>
-            <h3>Cronjob Monitoring</h3>
-            <IconButton icon={'sync'} onClick={sync} title="Sync"/>
-            <table
-                className={tableStyle}
-            >
-                <thead>
-                <tr
-                    className={css({
-                        borderBottom: '1px solid black',
-                    })}
-                >
-                    <th className={headerStyle}>Job Name</th>
-                    <th className={headerStyle}>Last Run Time</th>
-                </tr>
-                </thead>
-                <tbody>
-                {cronJobLogs.map(cronJobLog => (
-                    <CronJobRow key={cronJobLog.id} name={cronJobLog.jobName!} time={cronJobLog.lastRunTime!}/>
-                ))}
-                </tbody>
-            </table>
-        </div>
-    )
+  const sync = React.useCallback(() => {
+    dispatch(API.getCronJobLogs());
+  }, [dispatch]);
+
+  React.useEffect(() => {
+    if (data === 'NOT_INITIALIZED') {
+      sync();
+    }
+  }, [data, sync]);
+
+  return (
+    <div>
+      <h3>
+        Cronjob Monitoring <IconButton icon={'sync'} onClick={sync} title="Sync" />
+      </h3>
+      <div>
+        {typeof data === 'string' ? (
+          <AvailabilityStatusIndicator status={data} />
+        ) : data.length === 0 ? (
+          <div>
+            <em>no cronjob logs available</em>
+          </div>
+        ) : (
+          <table className={tableStyle}>
+            <thead>
+              <tr
+                className={css({
+                  borderBottom: '1px solid black',
+                })}
+              >
+                <th className={headerStyle}>Job Name</th>
+                <th className={headerStyle}>Last Run Time</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.map((cronJobLog: CronJobLog) => (
+                <CronJobRow
+                  key={cronJobLog.id}
+                  name={cronJobLog.jobName!}
+                  time={cronJobLog.lastRunTime!}
+                />
+              ))}
+            </tbody>
+          </table>
+        )}
+      </div>
+    </div>
+  );
 }
 
-function CronJobRow({name, time}: { name: string, time: number }): React.ReactElement {
-    const cronJobTime = new Date(0);
-    cronJobTime.setUTCMilliseconds(time);
+function CronJobRow({ name, time }: { name: string; time: number }): React.ReactElement {
+  const cronJobTime = new Date(0);
+  cronJobTime.setUTCMilliseconds(time);
 
-    return (
-        <tr>
-            <td>{name}</td>
-            <td>{cronJobTime.toLocaleString()}</td>
-        </tr>
-    )
+  return (
+    <tr>
+      <td>{name}</td>
+      <td>{cronJobTime.toLocaleString()}</td>
+    </tr>
+  );
 }
