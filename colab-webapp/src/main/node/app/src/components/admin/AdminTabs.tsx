5b3a8d9c8334146c66ffe23d66803b1c11227d42
Mikkel Vestergaard <56039404+TehAwol@users.noreply.github.com>
2026-04-13 13:49:34 +0200

    Revert to react-router-dom

--- colab-webapp/src/main/node/app/src/components/admin/AdminTabs.tsx
+++ colab-webapp/src/main/node/app/src/components/admin/AdminTabs.tsx
@@ -7,7 +7,7 @@
 
 import { css } from '@emotion/css';
 import * as React from 'react';
-import { useNavigate } from 'react-router';
+import { useNavigate } from 'react-router-dom';
 import useTranslations from '../../i18n/I18nContext';
 import { lightIconButtonStyle, space_xl } from '../../styling/style';
 import GlobalCardTypeList from '../cardtypes/GlobalCardTypeList';
